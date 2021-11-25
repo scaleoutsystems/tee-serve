c40dd5d456f18e92eaecc2ffac7f81d7c3c3ed55
mcapuccini <marco@scaleoutsystems.com>
2021-11-25 14:12:18 +0000

    client example

--- examples/cpp/client.cc
+++ examples/cpp/client.cc
@@ -4,15 +4,50 @@
 #include <grpcpp/create_channel.h>
 #include <grpcpp/security/credentials.h>
 
+#include "api.grpc.pb.h"
+
+using api::PredictionRequest;
+using api::PredictionResponse;
+using api::Predictor;
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
 class Client {
+  std::unique_ptr<Predictor::Stub> stub_;
+
  public:
-  Client(std::shared_ptr<Channel> channel, const std::string& db)
-      : stub_(RouteGuide::NewStub(channel)) {
-    routeguide::ParseDb(db, &feature_list_);
+  Client(std::shared_ptr<Channel> channel)
+      : stub_(Predictor::NewStub(channel)) {}
+
+  void PrintPrediction() {
+    // Setup context
+    ClientContext context;
+
+    // Setup request
+    PredictionRequest req;
+    req.set_input("testinput");
+
+    // Call service
+    PredictionResponse res;
+    Status status = stub_->Predict(&context, req, &res);
+
+    // Print out result if status OK
+    if (status.ok()) {
+      std::cout << "Prediction is: " << res.prediction() << std::endl;
+    } else {
+      std::cerr << "Server error" << std::endl;
+    }
   }
 };
 
 int main(int argc, char** argv) {
-  grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+  // Init client
+  std::shared_ptr<Channel> channel = grpc::CreateChannel(
+      "localhost:50051", grpc::InsecureChannelCredentials());
+  Client client(channel);
+
+  // Predict
+  client.PrintPrediction();
   return 0;
 }
