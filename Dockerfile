1bdcad6258c5d3ee56cd9676bb6663feb2ed7644
mcapuccini <marco@scaleoutsystems.com>
2021-12-06 15:03:18 +0000

    User/Agent container working

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM debian:bullseye-slim
+
+# Args
+ARG PRJ_NAME="mdn-poc"
+ARG TINI_VERSION="v0.19.0"
+
+# Copy
+COPY ./agent /usr/bin/agent
+COPY ./user /usr/bin/user
+COPY ./resources /resources
+
+# Install tini
+ADD https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini /usr/bin/tini
+RUN chmod +x /usr/bin/tini
+
+# Entrypoint
+ENTRYPOINT ["/usr/bin/tini"]
+CMD ["/usr/bin/agent"]
---
+++ bin/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Env
+PRJ_NAME="mdn-poc"
+DEVCNT_TAG="local/mdn-poc:devcontainer"
+CNT_TAG="local/mdn-poc:latest"
+
+# Build devcontainer
+>&2 echo "Building $DEVCNT_TAG" 
+[ -e agent ] && rm agent
+[ -e user ] && rm user
+docker build -t "$DEVCNT_TAG" . -f .devcontainer/Dockerfile
+
+# Extract bins
+>&2 echo "Extract binaries"
+docker container create --name "${PRJ_NAME}-extract" "$DEVCNT_TAG"
+docker container cp "${PRJ_NAME}-extract:/${PRJ_NAME}/build/agent" ./agent
+docker container cp "${PRJ_NAME}-extract:/${PRJ_NAME}/build/user" ./user
+docker container rm -f "${PRJ_NAME}-extract"
+
+# Build container
+>&2 echo "Building $DEVCNT_TAG" 
+docker build --no-cache -t "$CNT_TAG" .
