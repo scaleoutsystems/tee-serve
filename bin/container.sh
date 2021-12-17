0d1a93af0153a1a35c59fcdea130b7566267d3cd
mcapuccini <marco@scaleoutsystems.com>
2021-12-17 15:21:18 +0000

    add container launch command

--- bin/container.sh
+++ bin/container.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Build container
-docker build -f .devcontainer/Dockerfile -t tee-server .
+docker build \
+	-f .devcontainer/Dockerfile \
+	-t tee-server \
+	--build-arg DOCKER_USER=$(whoami) \
+	--build-arg USER_UID=$UID \
+	--build-arg $(id -u $USER) \
+	.
 
 # Run
-docker run --rm -it -v $PWD:/pwd -w /pwd --privileged -u default \
+docker run --rm -it -v $PWD:/pwd -w /pwd --privileged --net=host -u default \
 	tee-server /bin/bash
