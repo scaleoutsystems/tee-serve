0a22d71f4b294c990b2b01d4d781ec288db83289
mcapuccini <marco@scaleoutsystems.com>
2021-12-17 09:54:20 +0000

    build SGX gramine in docker

--- bin/build.sh
+++ bin/build.sh
@@ -5,8 +5,8 @@ set -e
 cmake --no-warn-unused-cli \
 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE \
 -DCMAKE_BUILD_TYPE:STRING=Debug \
--DCMAKE_C_COMPILER:FILEPATH=/usr/bin/x86_64-linux-gnu-gcc-10 \
--DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/x86_64-linux-gnu-g++-10 \
+-DCMAKE_C_COMPILER:FILEPATH=/usr/bin/x86_64-linux-gnu-gcc-11 \
+-DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/x86_64-linux-gnu-g++-11 \
 -H$PWD \
 -B$PWD/build \
 -G Ninja
